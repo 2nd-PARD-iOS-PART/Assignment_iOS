6b190a42ce75da4f725317af0da0b82035c0c0b2
syp0728 <syp0728@handong.ac.kr>
2023-11-04 20:29:08 +0900

    ScrollView

---
+++ ScrollView/ScrollView/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  ScrollView
+//
+//  Created by 박서윤 on 2023/11/04.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+        
+    }
+    
+}
